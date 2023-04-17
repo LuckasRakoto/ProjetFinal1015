5976b1565def56b597bc4804ee02ac55a2ce5429
GitHubYanis <yanis21p@gmail.com>
2023-04-17 15:21:44 -0400

    fenetre

--- king.hpp
+++ king.hpp
@@ -5,7 +5,7 @@
 class King : public Piece {
 public:
 	King(std::pair<int, int> position, PieceColor couleur) : Piece(position, 'K', couleur) {} // creation king (K)
-	King(PieceColor color, char piece) : Piece(color, 'K') {}
+	King(PieceColor color) : Piece(color, 'K') {}
 	~King() = default;
 
 	bool isValidMove(std::pair<int, int> newPosition, std::pair<int, int> currentPosition, Board& board);
