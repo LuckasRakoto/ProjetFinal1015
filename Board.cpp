85c660e9c1601aaa2a690d718809b3db5382b0c3
GitHubYanis <yanis21p@gmail.com>
2023-04-17 13:58:37 -0400

    board

--- Board.cpp
+++ Board.cpp
@@ -8,17 +8,17 @@ Board::Board() {
 		}
 	}
 
-	addPiece(new King({ 0,5 }, 'w'), 0, 5);
-	addPiece(new Bishop({ 0,2 }, 'w'), 0, 2);
-	addPiece(new Bishop({ 0,5 }, 'w'), 0, 5);
-	addPiece(new Knight({ 0,1 }, 'w'), 0, 1);
-	addPiece(new Knight({ 0,6 }, 'w'), 0, 6);
+	addPiece(new King({ 0,5 }, PieceColor::White), 0, 5);
+	addPiece(new Bishop({ 0,2 }, PieceColor::White), 0, 2);
+	addPiece(new Bishop({ 0,5 }, PieceColor::White), 0, 5);
+	addPiece(new Knight({ 0,1 }, PieceColor::White), 0, 1);
+	addPiece(new Knight({ 0,6 }, PieceColor::White), 0, 6);
 
-	addPiece(new King({ 7,5 }, 'b'), 0, 5);
-	addPiece(new Bishop({ 7,2 }, 'b'), 7, 2);
-	addPiece(new Bishop({ 7,5 }, 'b'), 7, 5);
-	addPiece(new Knight({ 7,1 }, 'b'), 7, 1);
-	addPiece(new Knight({ 7,6 }, 'b'), 7, 6);
+	addPiece(new King({ 7,5 }, PieceColor::Black), 0, 5);
+	addPiece(new Bishop({ 7,2 }, PieceColor::Black), 7, 2);
+	addPiece(new Bishop({ 7,5 }, PieceColor::Black), 7, 5);
+	addPiece(new Knight({ 7,1 }, PieceColor::Black), 7, 1);
+	addPiece(new Knight({ 7,6 }, PieceColor::Black), 7, 6);
 };
 
 void Board::addPiece(Piece* piece, int row, int col) {
