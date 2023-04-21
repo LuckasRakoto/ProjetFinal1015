6a466c8d4f204ff1b11901dd792dbd9033dc3346
Luckas Rakoto <luckasrakoto@gmail.com>
2023-04-20 20:18:32 -0400

    asd

--- ClickabelLabel.cpp
+++ ClickabelLabel.cpp
@@ -1,11 +1,14 @@
 #include "ClickabelLabel.hpp"
 
-ClickableLabel::ClickableLabel(QWidget* parent, Qt::WindowFlags f) :
-	QLabel(parent) {}
+ClickableLabel::ClickableLabel(QWidget* parent, Qt::WindowFlags f,int i,int j) :
+	QLabel(parent), row(i), col(j) {}
 
 
 ClickableLabel::~ClickableLabel() {}
 
 void ClickableLabel::mousePressEvent(QMouseEvent* event) {
-	emit clicked(4);
+	int i = row;
+	int j = col;
+	std::cout << i << std::endl;
+	emit clicked(i,j);
 }
--- ClickabelLabel.hpp
+++ ClickabelLabel.hpp
@@ -4,15 +4,19 @@
 #include <QLabel>
 #include <QWidget>
 #include <Qt>
+#include <iostream>
 
 class ClickableLabel : public QLabel {
 	Q_OBJECT
+private:
+	int col;
+	int row;
 public:
-	explicit ClickableLabel(QWidget* parent = Q_NULLPTR, Qt::WindowFlags f = Qt::WindowFlags());
+	explicit ClickableLabel(QWidget* parent = Q_NULLPTR, Qt::WindowFlags f = Qt::WindowFlags(),int i,int j);
 	~ClickableLabel();
 
 signals:
-	void clicked();
+	void clicked(int row, int col);
 
 protected:
 	void mousePressEvent(QMouseEvent* event);
